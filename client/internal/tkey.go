3f71ca8516e256b33f2c69989593deb73389f8bc
Simon Westlin Green <simon.green@live.se>
2025-02-12 14:19:34 +0100

    Add ability to sign challenges and send to application (#37)
    * moved tkey interface and signer
    
    * Moved things around and added ability to sign challenge
    
    * Refactored tkey helper and added errors
    
    * Removed unneeded prints

--- client/internal/tkey.go
+++ client/internal/tkey.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"crypto"
 	"crypto/ed25519"
 	"fmt"
 	"log"
@@ -14,36 +15,61 @@ const progname = "tkey-device-signer"
 
 var le = log.New(os.Stderr, "Error: ", 0)
 
-func GetTkeyPubKey() []byte {
-	signer := getSigner()
+func GetTkeyPubKey() ([]byte, error) {
+	signer, err := getSigner()
+
+	if err != nil {
+		return nil, err
+	}
 
 	pub, err := signer.tkSigner.GetPubkey()
 
 	if err != nil {
-		fmt.Println("Error getting Public Key")
-		return nil
+		return nil, err
 	}
 
 	sshPub, _ := ssh.NewPublicKey(ed25519.PublicKey(pub))
 
-	return ssh.MarshalAuthorizedKey(sshPub)
+	return ssh.MarshalAuthorizedKey(sshPub), nil
 }
 
-func getSigner() *Signer {
+func Sign(msg []byte) ([]byte, error) {
+
+	signer, err := getSigner()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !signer.connect() {
+		le.Printf("Connect failed")
+		return nil, fmt.Errorf("connect failed")
+	}
+
+	defer signer.disconnect()
+
+	sig, err := signer.Sign(nil, msg, crypto.Hash(0))
+	if err != nil {
+		le.Printf("Sign failed: %s\n", err)
+		return nil, err
+	}
+
+	return sig, nil
+}
+
+func getSigner() (*Signer, error) {
 	devPath, err := tkeyclient.DetectSerialPort(false)
 	if err != nil {
-		fmt.Println("Error detecting serial port")
-		return nil
+		return nil, err
 	}
 
 	serialSpeed := tkeyclient.SerialSpeed
 
 	exit := func(code int) {
-		fmt.Println("Error connecting to TKEY")
 		os.Exit(0)
 	}
 
 	signer := NewSigner(devPath, serialSpeed, false, "", "", exit)
 
-	return signer
+	return signer, nil
 }
