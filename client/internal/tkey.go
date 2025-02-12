485f595a823a1ad2ad0ee0519afd9337e8c00f5e
Simon Westlin Green <simon.green@live.se>
2025-02-12 14:03:24 +0100

    moved tkey interface and signer (#35)
    * moved tkey interface and signer
    
    * Moved signer into own func

---
+++ client/internal/tkey.go
@@ -0,0 +1,49 @@
+package internal
+
+import (
+	"crypto/ed25519"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/tillitis/tkeyclient"
+	"golang.org/x/crypto/ssh"
+)
+
+const progname = "tkey-device-signer"
+
+var le = log.New(os.Stderr, "Error: ", 0)
+
+func GetTkeyPubKey() []byte {
+	signer := getSigner()
+
+	pub, err := signer.tkSigner.GetPubkey()
+
+	if err != nil {
+		fmt.Println("Error getting Public Key")
+		return nil
+	}
+
+	sshPub, _ := ssh.NewPublicKey(ed25519.PublicKey(pub))
+
+	return ssh.MarshalAuthorizedKey(sshPub)
+}
+
+func getSigner() *Signer {
+	devPath, err := tkeyclient.DetectSerialPort(false)
+	if err != nil {
+		fmt.Println("Error detecting serial port")
+		return nil
+	}
+
+	serialSpeed := tkeyclient.SerialSpeed
+
+	exit := func(code int) {
+		fmt.Println("Error connecting to TKEY")
+		os.Exit(0)
+	}
+
+	signer := NewSigner(devPath, serialSpeed, false, "", "", exit)
+
+	return signer
+}
