cba277e0c8e6f68568d66c9835e65516a4a68c21
IsacSnecker <isac.snecker@hotmail.com>
2025-02-13 12:37:34 +0100

    Refactored main.go

--- client/client.go
+++ client/client.go
@@ -13,38 +13,19 @@ func main() {
 
 	// Gets mode from user inputs and runs selected mode. Loops until program is told to exit.
 	for {
-		mode := select_mode()
+		mode := internal.Select_mode()
 
 		switch mode {
 		case 1:
 			// Perform register
 		case 2:
 			// Perform login
-			call_login()
+			internal.Call_login()
 		case 3:
 			// Stop program
 			return
 		default:
 			fmt.Println("Invalid choice, please try again.")
 		}
-	}
-}
-
-func select_mode() int {
-	fmt.Println("Select Mode:")
-	fmt.Println("1. Register")
-	fmt.Println("2. Login")
-	fmt.Println("3. Exit")
-
-	var choice int
-	fmt.Print("Enter choice (1/2/3): ")
-	fmt.Scanln(&choice)
-	return choice
-}
-
-func call_login() {
-	err := internal.Login("user")
-	if err != nil {
-		le.Println(err)
 	}
 }
---
+++ client/internal/mode_selector.go
@@ -0,0 +1,24 @@
+package internal
+
+import (
+	"fmt"
+)
+
+func Select_mode() int {
+	fmt.Println("Select Mode:")
+	fmt.Println("1. Register")
+	fmt.Println("2. Login")
+	fmt.Println("3. Exit")
+
+	var choice int
+	fmt.Print("Enter choice (1/2/3): ")
+	fmt.Scanln(&choice)
+	return choice
+}
+
+func Call_login() {
+	err := Login("user")
+	if err != nil {
+		le.Println(err)
+	}
+}
