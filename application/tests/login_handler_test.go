b3569e22c520ee8e59dddb033b7311835bb4d003
almstromjonas <jonasalm@chalmers.se>
2025-02-17 08:21:51 +0100

    test documentation

--- application/tests/login_handler_test.go
+++ application/tests/login_handler_test.go
@@ -12,10 +12,19 @@ import (
 	"chalmers/tkey-group22/application/internal"
 )
 
+// This test file could be changed to test all handlers.
+
 const usersFilePath = "../data/users.csv"
 const backupFilePath = "../data/users_backup.csv"
+const loginURL = "/api/login"
 
-// Helper function to backup the existing users.csv file
+// backupUsersFile backs up the existing users.csv file to users_backup.csv.
+//
+// Parameters:
+//   - t: The testing.T instance.
+//
+// Returns:
+//   - None
 func backupUsersFile(t *testing.T) {
 	input, err := os.ReadFile(usersFilePath)
 	if err != nil {
@@ -28,7 +37,13 @@ func backupUsersFile(t *testing.T) {
 	}
 }
 
-// Helper function to restore the original users.csv file from the backup
+// restoreUsersFile restores the original users.csv file from the backup.
+//
+// Parameters:
+//   - t: The testing.T instance.
+//
+// Returns:
+//   - None
 func restoreUsersFile(t *testing.T) {
 	input, err := os.ReadFile(backupFilePath)
 	if err != nil {
@@ -46,7 +61,14 @@ func restoreUsersFile(t *testing.T) {
 	}
 }
 
-// Helper function to write mock data to users.csv file
+// writeMockData writes mock data to the users.csv file.
+//
+// Parameters:
+//   - t: The testing.T instance.
+//   - data: A 2D slice of strings representing the mock data to be written.
+//
+// Returns:
+//   - None
 func writeMockData(t *testing.T, data [][]string) {
 	file, err := os.Create(usersFilePath)
 	if err != nil {
@@ -64,11 +86,22 @@ func writeMockData(t *testing.T, data [][]string) {
 	}
 }
 
+// TestMain sets up the test environment, runs the tests, and then restores the
+// original environment.
+//
+// Parameters:
+//   - m: The testing.M instance.
+//
+// Returns:
+//   - None
+//
+// Dependencies:
+//   - backupUsersFile
+//   - writeMockData
+//   - restoreUsersFile
 func TestMain(m *testing.M) {
-	// Backup the existing users.csv file
 	backupUsersFile(nil)
 
-	// Write mock data to users.csv file
 	mockData := [][]string{
 		{"username", "publickey"},
 		{"bob", "mocked_banana1234"},
@@ -76,16 +109,25 @@ func TestMain(m *testing.M) {
 	}
 	writeMockData(nil, mockData)
 
-	// Run the tests
+	// Run the tests including helper funcs
 	code := m.Run()
 
-	// Restore the original users.csv file
 	restoreUsersFile(nil)
 
 	os.Exit(code)
 }
 
-// Helper function to create a new HTTP request and recorder
+// createRequest creates a new HTTP request and response recorder for testing.
+//
+// Parameters:
+//   - t: The testing.T instance.
+//   - method: The HTTP method.
+//   - url: The URL for the request.
+//   - body: A map representing the JSON body of the request.
+//
+// Returns:
+//   - *httptest.ResponseRecorder: The response recorder for capturing the response.
+//   - *http.Request: The created HTTP request.
 func createRequest(t *testing.T, method, url string, body map[string]string) (*httptest.ResponseRecorder, *http.Request) {
 	requestBody, err := json.Marshal(body)
 	if err != nil {
@@ -103,7 +145,7 @@ func createRequest(t *testing.T, method, url string, body map[string]string) (*h
 
 // Valid input. Expects success.
 func TestLoginHandler_Success(t *testing.T) {
-	rr, req := createRequest(t, http.MethodGet, "/api/login", map[string]string{"username": "bob"})
+	rr, req := createRequest(t, http.MethodGet, loginURL, map[string]string{"username": "bob"})
 	internal.LoginHandler(rr, req)
 
 	if status := rr.Code; status != http.StatusOK {
@@ -118,7 +160,7 @@ func TestLoginHandler_Success(t *testing.T) {
 
 // Invalid method. Expects fail.
 func TestLoginHandler_InvalidMethod(t *testing.T) {
-	rr, req := createRequest(t, http.MethodPost, "/api/login", nil)
+	rr, req := createRequest(t, http.MethodPost, loginURL, nil)
 	internal.LoginHandler(rr, req)
 
 	if status := rr.Code; status != http.StatusMethodNotAllowed {
@@ -128,7 +170,7 @@ func TestLoginHandler_InvalidMethod(t *testing.T) {
 
 // Invalid req body. Expects fail.
 func TestLoginHandler_InvalidRequestBody(t *testing.T) {
-	rr, req := createRequest(t, http.MethodGet, "/api/login", map[string]string{"invalid": "body"})
+	rr, req := createRequest(t, http.MethodGet, loginURL, map[string]string{"invalid": "body"})
 	internal.LoginHandler(rr, req)
 
 	if status := rr.Code; status != http.StatusBadRequest {
@@ -138,7 +180,7 @@ func TestLoginHandler_InvalidRequestBody(t *testing.T) {
 
 // No username in req body. Expects fail.
 func TestLoginHandler_UsernameNotProvided(t *testing.T) {
-	rr, req := createRequest(t, http.MethodGet, "/api/login", map[string]string{})
+	rr, req := createRequest(t, http.MethodGet, loginURL, map[string]string{})
 	internal.LoginHandler(rr, req)
 
 	if status := rr.Code; status != http.StatusBadRequest {
@@ -148,7 +190,7 @@ func TestLoginHandler_UsernameNotProvided(t *testing.T) {
 
 // Bad user. Expects fail.
 func TestLoginHandler_UserNotFound(t *testing.T) {
-	rr, req := createRequest(t, http.MethodGet, "/api/login", map[string]string{"username": "nonexistent"})
+	rr, req := createRequest(t, http.MethodGet, loginURL, map[string]string{"username": "nonexistent"})
 	internal.LoginHandler(rr, req)
 
 	if status := rr.Code; status != http.StatusNotFound {
