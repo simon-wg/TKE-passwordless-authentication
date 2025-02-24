7faddaf8be2345113b852b64aeb75f4f12bffc71
Flaskhals68 <03kajo@gmail.com>
2025-02-24 16:14:00 +0100

    Make agent active client program

--- client/cmd/agent.go
+++ client/cmd/agent.go
@@ -1,26 +1,5 @@
 package main
 
-<<<<<<< Updated upstream
-// // import (
-// // 	"chalmers/tkey-group22/internal/tkey"
-// // 	"encoding/json"
-// // 	"fmt"
-// // 	"io/ioutil"
-// // 	"net/http"
-// // )
-
-// // func enableCORS(next http.Handler) http.Handler {
-// // 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// // 		w.Header().Set("Access-Control-Allow-Origin", "*")
-// // 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-// // 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-// // 		if r.Method == "OPTIONS" {
-// // 			return
-// // 		}
-// // 		next.ServeHTTP(w, r)
-// // 	})
-// // }
-=======
 import (
 	"chalmers/tkey-group22/internal/tkey"
 	"encoding/hex"
@@ -29,7 +8,14 @@ import (
 	"net/http"
 )
 
-// CORS middleware function
+func main() {
+	http.Handle("/api/getTkeyPubKey", enableCors(http.HandlerFunc(getTkeyPubKeyHandler)))
+	http.Handle("/api/sign", enableCors(http.HandlerFunc(signHandler)))
+
+	fmt.Println("Server running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
 func enableCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -43,113 +29,17 @@ func enableCors(next http.Handler) http.Handler {
 	})
 }
 
-func main() {
-	http.Handle("/api/getTkeyPubKey", enableCors(http.HandlerFunc(getTkeyPubKeyHandler)))
-	http.Handle("/api/sign", enableCors(http.HandlerFunc(signHandler)))
->>>>>>> Stashed changes
+func getTkeyPubKeyHandler(w http.ResponseWriter, r *http.Request) {
+	pubKey, err := tkey.GetTkeyPubKey()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to get public key: %v", err), http.StatusInternalServerError)
+		return
+	}
 
-// // func main() {
-// // 	http.HandleFunc("/api/getTkeyPubKey", getTkeyPubKeyHandler)
-// // 	http.HandleFunc("/api/sign", signHandler)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"publicKey": string(pubKey)})
+}
 
-// // 	enableCORS(mux)
-
-// // 	fmt.Println("Server running on http://localhost:8080")
-// // 	http.ListenAndServe(":8080", nil)
-// // }
-
-<<<<<<< Updated upstream
-// // func getTkeyPubKeyHandler(w http.ResponseWriter, r *http.Request) {
-// // 	pubKey, err := tkey.GetTkeyPubKey()
-// // 	if err != nil {
-// // 		http.Error(w, fmt.Sprintf("Failed to get public key: %v", err), http.StatusInternalServerError)
-// // 		return
-// // 	}
-
-// // 	w.Header().Set("Content-Type", "application/json")
-// // 	json.NewEncoder(w).Encode(map[string]string{"publicKey": string(pubKey)})
-// // }
-
-// // func signHandler(w http.ResponseWriter, r *http.Request) {
-// // 	body, err := ioutil.ReadAll(r.Body)
-// // 	if err != nil {
-// // 		http.Error(w, "Invalid request body", http.StatusBadRequest)
-// // 		return
-// // 	}
-
-// // 	sig, err := tkey.Sign(body)
-// // 	if err != nil {
-// // 		http.Error(w, fmt.Sprintf("Failed to sign message: %v", err), http.StatusInternalServerError)
-// // 		return
-// // 	}
-
-// // 	w.Header().Set("Content-Type", "application/json")
-// // 	json.NewEncoder(w).Encode(map[string]string{"signature": string(sig)})
-// // }
-
-// package tkey
-
-// import (
-// 	"chalmers/tkey-group22/internal/tkey"
-// 	"encoding/json"
-// 	"fmt"
-// 	"io/ioutil"
-// 	"net/http"
-// )
-
-// // CORS middleware
-// func enableCORS(next http.Handler) http.Handler {
-// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		w.Header().Set("Access-Control-Allow-Origin", "*")
-// 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-// 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-// 		if r.Method == "OPTIONS" {
-// 			return
-// 		}
-// 		next.ServeHTTP(w, r)
-// 	})
-// }
-
-// func main() {
-// 	mux := http.NewServeMux()
-// 	mux.HandleFunc("/api/getTkeyPubKey", getTkeyPubKeyHandler)
-// 	mux.HandleFunc("/api/sign", signHandler)
-
-// 	// Apply the CORS middleware
-// 	handler := enableCORS(mux)
-
-// 	fmt.Println("Server running on http://localhost:8080")
-// 	http.ListenAndServe(":8080", handler)
-// }
-
-// func getTkeyPubKeyHandler(w http.ResponseWriter, r *http.Request) {
-// 	pubKey, err := tkey.GetTkeyPubKey()
-// 	if err != nil {
-// 		http.Error(w, fmt.Sprintf("Failed to get public key: %v", err), http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	w.Header().Set("Content-Type", "application/json")
-// 	json.NewEncoder(w).Encode(map[string]string{"publicKey": string(pubKey)})
-// }
-
-// func signHandler(w http.ResponseWriter, r *http.Request) {
-// 	body, err := ioutil.ReadAll(r.Body)
-// 	if err != nil {
-// 		http.Error(w, "Invalid request body", http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	sig, err := tkey.Sign(body)
-// 	if err != nil {
-// 		http.Error(w, fmt.Sprintf("Failed to sign message: %v", err), http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	w.Header().Set("Content-Type", "application/json")
-// 	json.NewEncoder(w).Encode(map[string]string{"signature": string(sig)})
-// }
-=======
 func signHandler(w http.ResponseWriter, r *http.Request) {
 	var requestBody map[string]string
 	if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
@@ -180,4 +70,3 @@ func signHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{"signature": signatureHex})
 }
->>>>>>> Stashed changes
