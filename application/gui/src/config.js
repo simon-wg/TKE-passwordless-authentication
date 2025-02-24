ab55c71ebefed54537ecda9a6467d42681488778
Flaskhals <77638480+Flaskhals68@users.noreply.github.com>
2025-02-24 17:19:52 +0100

    Make client new agent

--- application/gui/src/config.js
+++ application/gui/src/config.js
@@ -1,6 +1,6 @@
 const config = {
     serverBaseUrl: 'http://192.168.50.106:8080',
-    agentBaseUrl: 'http://localhost:6060'
+    clientBaseUrl: 'http://localhost:6060'
   };
   
 export default config;
