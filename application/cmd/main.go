218ce46b08fb3fa3d960f54042f847b7f15713c9
Flaskhals <77638480+Flaskhals68@users.noreply.github.com>
2025-02-26 18:39:12 +0100

    Restore files before merge

--- application/cmd/main.go
+++ application/cmd/main.go
@@ -6,29 +6,11 @@ import (
 	"net/http"
 )
 
-// CORS middleware
-func enableCORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/api/register", internal.RegisterHandler)
-	mux.HandleFunc("/api/login", internal.LoginHandler)
-	mux.HandleFunc("/api/verify", internal.VerifyHandler)
+	http.HandleFunc("/api/register", internal.RegisterHandler)
+	http.HandleFunc("/api/login", internal.LoginHandler)
+	http.HandleFunc("/api/verify", internal.VerifyHandler)
 
-	// Apply the CORS middleware
-	handler := enableCORS(mux)
-
-	fmt.Println("Server running on http://localhost:8080")
-	http.ListenAndServe(":8080", handler)
+	fmt.Println("Mock application running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
--- client/internal/util/mode_selector.go
+++ client/internal/util/mode_selector.go
@@ -1,12 +1,14 @@
 package util
 
 import (
+	"chalmers/tkey-group22/internal/auth"
 	"fmt"
 	"log"
 	"os"
 )
 
 var le = log.New(os.Stderr, "Error: ", 0)
+var appurl = "http://localhost:8080"
 
 func SelectMode() int {
 	fmt.Println("\nSelect Mode:")
@@ -21,15 +23,17 @@ func SelectMode() int {
 }
 
 func CallLogin() {
-	// err := auth.Login()
-	// if err != nil {
-	// 	le.Println(err)
-	// }
+	username := auth.GetUsername()
+	err := auth.Login(appurl, username)
+	if err != nil {
+		le.Println(err)
+	}
 }
 
 func CallRegister() {
-	// err := auth.Register()
-	// if err != nil {
-	// 	le.Println(err)
-	// }
+	username := auth.GetUsername()
+	err := auth.Register(appurl, username)
+	if err != nil {
+		le.Println(err)
+	}
 }
