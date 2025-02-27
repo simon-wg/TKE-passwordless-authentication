0e23f3e22c6db93d931da79d8b9f4bf582100168
Jonas Almström <112483701+almstromjonas@users.noreply.github.com>
2025-02-27 15:07:56 +0100

    67 prompt the user for uss on register and login (#93)
    * user gets prompted to input optional uss
    
    * reuse existing signer if it is already loaded onto the tkey

--- client/internal/tkey/tkey.go
+++ client/internal/tkey/tkey.go
@@ -1,11 +1,13 @@
 package tkey
 
 import (
+	"bufio"
 	"crypto"
 	"crypto/ed25519"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/tillitis/tkeyclient"
 )
@@ -13,6 +15,7 @@ import (
 const progname = "tkey-device-signer"
 
 var le = log.New(os.Stderr, "Error: ", 0)
+var existingSigner *Signer
 
 func GetTkeyPubKey() (ed25519.PublicKey, error) {
 	signer, err := getSigner()
@@ -66,6 +69,11 @@ func Sign(msg []byte) ([]byte, error) {
 }
 
 func getSigner() (*Signer, error) {
+	if existingSigner != nil && existingSigner.connect() && existingSigner.isWantedApp() {
+		// The signer app is already loaded, return the existing signer
+		return existingSigner, nil
+	}
+
 	devPath, err := tkeyclient.DetectSerialPort(false)
 	if err != nil {
 		return nil, err
@@ -77,7 +85,24 @@ func getSigner() (*Signer, error) {
 		os.Exit(0)
 	}
 
-	signer := NewSigner(devPath, serialSpeed, false, "", "", exit)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Do you want to enter a manual User Supplied Secret (USS) or provide a USS file? (m/f/n): ")
+	response, _ := reader.ReadString('\n')
+	response = strings.TrimSpace(strings.ToLower(response))
+
+	enterUSS := false
+	fileUSS := ""
+
+	if response == "m" {
+		enterUSS = true
+	} else if response == "f" {
+		fmt.Print("Please provide the path to the USS file: ")
+		fileUSS, _ = reader.ReadString('\n')
+		fileUSS = strings.TrimSpace(fileUSS)
+	}
+
+	signer := NewSigner(devPath, serialSpeed, enterUSS, fileUSS, "", exit)
+	existingSigner = signer
 
 	return signer, nil
 }
