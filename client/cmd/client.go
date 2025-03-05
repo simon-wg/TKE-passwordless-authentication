d760553bd45001fc61df38e672506308a915a894
epicreach <luqas.lundahl4817@gmail.com>
2025-03-05 23:03:31 +0100

    fixed bug in register

--- client/cmd/client.go
+++ client/cmd/client.go
@@ -59,7 +59,10 @@ func startWebClient() {
 
 func enableCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+		origin := r.Header.Get("Origin")
+		if origin == "http://localhost:8080" || origin == "http://localhost:3000" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
