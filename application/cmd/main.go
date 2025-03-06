7efab7ffc030cdca4b3db57c1d1715bc1a49d725
Simon Westlin Green <simon.green@live.se>
2025-03-06 14:00:56 +0100

    Use same allow origin in client as application

--- application/cmd/main.go
+++ application/cmd/main.go
@@ -38,7 +38,10 @@ func main() {
 
 func enableCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		origin := r.Header.Get("Origin")
+		if origin == "http://localhost:8080" || origin == "http://localhost:3000" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
