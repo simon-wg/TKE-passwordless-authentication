b1042fccf38bf9606d6f58e6809e85b7d1fc6a3a
Simon Westlin Green <simon.green@live.se>
2025-03-06 13:04:39 +0100

    changed getuser endpoint missing

--- application/cmd/main.go
+++ application/cmd/main.go
@@ -38,7 +38,7 @@ func main() {
 
 func enableCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
