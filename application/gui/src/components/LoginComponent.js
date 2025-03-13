46ae254b18f620a8682b30ba043dc323c59fc1e3
Isac Snecker <isac.snecker@hotmail.com>
2025-03-13 18:55:25 +0100

    Change fetch request

--- application/gui/src/components/LoginComponent.js
+++ application/gui/src/components/LoginComponent.js
@@ -17,8 +17,8 @@ const LoginComponent = () => {
     event.preventDefault();
     clearMessages();
     setLoading(true);
-    // Send POST Request to localhost:8080/api/initialize-login
-    const response = await fetch("http://localhost:8080/api/initialize-login", {
+    // Send POST Request to client /api/login endpoint
+      const response = await fetch(config.clientBaseUrl + "/api/login", {      
       method: "POST",
       headers: {
         "Content-Type": "application/json",
