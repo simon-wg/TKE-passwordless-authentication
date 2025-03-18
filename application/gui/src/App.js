38c168664b52e2ce44080a12a0b18f78044334cd
Flaskhals <77638480+Flaskhals68@users.noreply.github.com>
2025-03-18 15:52:43 +0100

    Remove old version of App.js

--- application/gui/src/App.js
+++ application/gui/src/App.js
@@ -1,35 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import RegisterComponent from "./components/RegisterComponent";
-// import { Routes, Route } from "react-router-dom";
-// import LoginComponent from "./components/LoginComponent";
-// import Navbar from "./components/Navbar";
-// import "./components/styles.css";
-// import NotesApp from "./components/NotesApp";
-// import useFetchUser from "./hooks/useFetchUser";
-
-// const App = () => {
-//   const [page, setPage] = useState("register");
-//   const user = useFetchUser();
-
-//   useEffect(() => {
-//     if (user !== null) {
-//       setPage("app");
-//     }
-//   }, [user]);
-
-//   return (
-//     <div>
-//       <Navbar setPage={setPage} currentPage={page} />
-//       {page === "register" && <RegisterComponent />}
-//       {page === "login" && <LoginComponent />}
-//       {page === "app" && <NotesApp />}
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
 import React, { useState, useEffect } from "react";
 import RegisterComponent from "./components/RegisterComponent";
 import { Routes, Route } from "react-router-dom";
