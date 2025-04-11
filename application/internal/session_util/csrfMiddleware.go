1d5b18fea754728ecf5d615b585c28eca716e4f9
epicreach <luqas.lundahl4817@gmail.com>
2025-04-11 15:00:36 +0200

    set csrf https to secure

--- application/internal/session_util/csrfMiddleware.go
+++ application/internal/session_util/csrfMiddleware.go
@@ -4,7 +4,7 @@ import "github.com/gorilla/csrf"
 
 var CsrfMiddleware = csrf.Protect(
 	[]byte("your-32-byte-secret-key-here"),
-	csrf.Secure(false), // true in production with HTTPS
+	csrf.Secure(true), // true in production with HTTPS
 	csrf.Path("/"),
 	csrf.HttpOnly(true),
 	csrf.SameSite(csrf.SameSiteLaxMode),
