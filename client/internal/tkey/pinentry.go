66afc5f8ba1b666fc5c7e9d378e2574108774899
Carl <carlforssell@hotmail.com>
2025-04-23 14:37:23 +0200

    Fix uss text (#167)
    * Changed text asking for USS
    
    * Layout changes of text

--- client/internal/tkey/pinentry.go
+++ client/internal/tkey/pinentry.go
@@ -21,9 +21,10 @@ import (
 func getSecret(udi string, pinentryProgram string) ([]byte, error) {
 	// Displaying the Unique Device Identifier (UDI) so the user will
 	// know which stick they have plugged in.
-	desc := fmt.Sprintf("%s needs a User Supplied Secret\n"+
+	desc := fmt.Sprintf("%s: Please enter an optional User Supplied Secret "+
 		"(USS) for your TKey with number:\n"+
-		"%v", progname, udi)
+		"%v\n"+
+		"\nLeave the field blank if you do not want to use a USS.", progname, udi)
 
 	if runtime.GOOS == "darwin" && pinentryProgram == "" {
 		pin, err := macOSPrompt(desc, progname)
