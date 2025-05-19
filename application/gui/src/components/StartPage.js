1cdf6c4b64942730fcd8acd4a8dda45d7ad3f054
Luqas <118449875+epicreach@users.noreply.github.com>
2025-05-19 15:21:24 +0200

    173 improve UI (#175)
    * improved UI
    
    * removed link
    
    * tweaked wording in instructions

--- application/gui/src/components/StartPage.js
+++ application/gui/src/components/StartPage.js
@@ -1,34 +1,46 @@
-import React from 'react';
-import './StartPage.css';
+import React from "react";
+import "./StartPage.css";
 
 const StartPage = ({ setPage }) => {
-    const handleRegisterClick = () => {
-        setPage("register");
-    };
+  const handleRegisterClick = () => {
+    setPage("register");
+  };
 
-    return (
-        <div style={{ textAlign: 'center', padding: '20px' }}>
-            <h1>Welcome to the TKey Login Demo</h1>
-            <p>
-                This website demonstrates a passwordless authentication system using TKey. 
-            </p>
-            <p>
-                Make sure the client service is running on your device before continuing.
-            </p>
-            <p>
-                When the client is running, plug in your TKey, head to the register or login page and fill out the form as usual.
-            </p>
-            <p>
-                The first time registering or login in after plugging in you will be prompted by an optional user supplied secret (password)."
-            </p>
-            <p>
-                Then when the TKey starts to blink green, give it a touch!
-            </p>
-            <button className='to-register-btn' onClick={handleRegisterClick}>
-                Register with TKey
-            </button>
-        </div>
-    );
+  return (
+    <div className="centered-container">
+      {" "}
+      <div className="get-started-box">
+        {" "}
+        <h1 style={{ textAlign: "center" }}>
+          Welcome to the TKey Login Demo
+        </h1>{" "}
+        <div className="get-started-container">
+          {" "}
+          <h2>Getting Started</h2>{" "}
+          <ul>
+            {" "}
+            <li> Make sure you have the daemon installed and running </li>{" "}
+            <li>Plug in your TKey</li>
+            <li>Go to the Register page and fill out the forms</li>{" "}
+            <li>
+              {" "}
+              When registering or logging in, you will be prompted to enter an
+              optional User Supplied Secret (password){" "}
+            </li>{" "}
+            <li>
+              {" "}
+              If you've created an account with an USS you will need to enter
+              the same USS each time you login{" "}
+            </li>{" "}
+          </ul>{" "}
+        </div>{" "}
+        <button className="to-register-btn" onClick={handleRegisterClick}>
+          {" "}
+          Register with TKey{" "}
+        </button>{" "}
+      </div>{" "}
+    </div>
+  );
 };
 
 export default StartPage;
